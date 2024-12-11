ab39babe35bee96781ff2b506426c44f306aa484
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-12-11 20:04:10 +0800

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import banner2 from "../assets/banner2.png";
+import banner2 from "../assets/roadmapdizzy.mp4";
 
 const About = () => {
   return (
@@ -7,15 +7,14 @@ const About = () => {
       <div className="container flex flex-col justify-center items-center text-center ">
         <article className="flex justify-center items-center flex-col text-center gap-8">
           <h1 className="text-7xl md:text-8xl font-MangoMed font-bold uppercase md:w-[700px] about-header">
-            "FROM <span className="text-purple-300 ">THIN</span> AIR."
+            "FROM <span className="text-purple-300 ">FRESH</span> WATER."
           </h1>
 
           <div className="relative w-full  flex flex-col gap-10">
             <p className="text-xl max-w-[900px] about-para">
-              $SPELL is a mysterious token created from thin air. The secret to
-              this riddle is closing your eyes while saying "Abracadabra" three
-              times in the mirror while holding a gold bar and 1 BTC to make our
-              mystical $SPELL work.
+              The enigmatic Pokémon-inspired card token has 
+              emerged from the shadows onto the SUI network. To unlock its true powers, you must whisper "Dizzy Beam Activate" three times under a crescent moon while holding 
+              a rare $DIZZY card and a holographic Pokéball. Only then will the mystical energies of $DIZZY reveal themselves!
             </p>
 
             <img src={banner2} alt="" className="" loading="lazy" />
